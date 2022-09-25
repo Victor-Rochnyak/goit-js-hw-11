81b57c1dc62e87a93b37f60cbcf318288daeb74c
Victor-Rochnyak <victor.rochnyak@gmail.com>
2022-09-26 00:55:50 +0300

    дз11

--- src/js/components/news-api-service.js
+++ src/js/components/news-api-service.js
@@ -17,7 +17,7 @@ export default class NewsApiService {
         orientation: 'horizontal',
         safesearch: true,
         page: `${this.page}`,
-        per_page: 5,
+        per_page: 40,
       },
     };
     try {
